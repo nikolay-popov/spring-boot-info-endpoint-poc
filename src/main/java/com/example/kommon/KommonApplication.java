824ba305d3f9de03912f45144250f736c81b1bee
Nikolay Popov <nikolay.popov@verygoodsecurity.com>
2018-03-20 19:53:13 +0700

    Commit src

---
+++ src/main/java/com/example/kommon/KommonApplication.java
@@ -0,0 +1,12 @@
+package com.example.kommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KommonApplication.class, args);
+	}
+}
