155af4661db94be069d6cb72482c4633f6a3cf9a
Nikolay Popov <nikolay.popov@verygoodsecurity.com>
2018-03-21 17:43:55 +0700

    Comments

--- src/main/java/com/example/kommon/SecurityConfig.java
+++ src/main/java/com/example/kommon/SecurityConfig.java
@@ -16,9 +16,12 @@ import java.util.Collections;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+  // not overriding configure methods - all endpoints are secured by default
+
   @Bean
   @Override
   public UserDetailsService userDetailsService() {
+    // adding a user - primary for tests
     UserDetails user = new User("user", "pwd", Collections.emptyList());
 
     return new InMemoryUserDetailsManager(new ArrayList<UserDetails>() {{ add(user); }});
